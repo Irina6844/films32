1deb71c71cd0e8c15f09a3aa758f3a7e4b45d57e
Irina6844 <irinasargsyan3@gmail.com>
2025-12-13 21:00:03 +0400

    styles

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { getFilmsListThunk } from "../../../store/slices/filmSlice";
 import { useAppDispatch, useAppSelector } from "../../../store/hooks";
 import { Box, Typography } from "@mui/material";
 
-
-const imgUrl = "https://image.tmdb.org/t/p/w500/"
+const imgUrl = "https://image.tmdb.org/t/p/w400/";
 const Home = () => {
   const { films } = useAppSelector((state) => state.filmsData);
   const dispatch = useAppDispatch();
@@ -12,14 +11,49 @@ const Home = () => {
     dispatch(getFilmsListThunk());
   }, []);
 
-
+  
   return (
-    <Box>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        // alignItems: "center",
+        gap: "15px",
+        flexWrap: "wrap",
+        mt: "20px",
+      }}
+    >
       {films.map((film) => {
         return (
-          <Box>
+          <Box
+            sx={{
+              border: "3px solid #263a69ff",
+              padding: "10px 20px",
+              textAlign: "center",
+              borderRadius: "30px",
+              maxWidth: "400px",
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "20px",
+              transition: "all 0.5s",
+
+              "&:hover": {
+                transform: "translateY(-10px)",
+
+                "& img": {
+                  transform: "scale(1.07)",
+                },
+              },
+            }}
+          >
             <Typography variant="h4">{film.title}</Typography>
-            <img src={imgUrl + film.poster_path}/>
+            <img
+              src={imgUrl + film.poster_path}
+              style={{ borderRadius: "30px", transition: "all 0.5s" }}
+            />
           </Box>
         );
       })}
